c908f8578d465123262ae823a1b9e4c312df32c1
Pepe Galan Chiner <pepe.galan.chiner@gmail.com>
2026-02-28 21:32:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4090218871-bed57",
+  "appId": "1:497896328326:web:a9fb2083db1d7db5ad1507",
+  "apiKey": "AIzaSyAXeHl6ihLOY9g3w9SiGCthJEarFgZQOdA",
+  "authDomain": "studio-4090218871-bed57.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "497896328326"
+};
